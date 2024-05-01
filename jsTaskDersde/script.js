e058eb281e4adf4b96f2f69d8f53978b29dcb575
Ismayil <ismayilteymurov.444@gmail.com>
2024-05-01 17:44:31 +0400

    evde davam et

---
+++ jsTaskDersde/script.js
@@ -0,0 +1,17 @@
+const input = document.querySelector(".input")
+const add = document.querySelector(".add")
+const list = document.querySelector(".list")
+add.addEventListener("click", function(){
+    
+    const allList = document.createElement('li')
+    allList.style.listStyle="delteBox"
+    
+    allList.innerText=input.value
+    
+    const deleteBox = document.createElement('button')
+    deleteBox.classList.add('deleteBox')
+    allList.append(deleteBox)
+    list.append(allList)
+  
+})
+
