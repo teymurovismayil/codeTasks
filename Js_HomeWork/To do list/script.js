2ce38ae1ebd4d46c31cbb16ed0bf8921aa7d86c8
tymrvi <ismayilteymurov.444@gmail.com>
2024-05-04 21:17:18 +0400

    todo list

--- Js_HomeWork/To do list/script.js
+++ Js_HomeWork/To do list/script.js
@@ -1,19 +1,25 @@
 const input = document.querySelector(".input")
 const add = document.querySelector(".add")
 const list = document.querySelector(".list")
+const deleteBtn = document.querySelector(".deleteBtn")
+
 
 add.addEventListener("click", function(){
-    
+  
     const allList = document.createElement('li')
     allList.style.listStyle="none"
     
     allList.innerText=input.value
     
     const deleteBox = document.createElement('button')
-    deleteBox.classList.add('deleteBox')
+    deleteBox.classList.add('deleteBtn')
     allList.append(deleteBox)
     list.append(allList)
+    deleteBox.addEventListener("click", function(){
+        allList.remove()
+    })
   
 })
+
 
 
